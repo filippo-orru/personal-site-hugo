9ee6dbfacb067356aa34d4619c518d2223e55f49
Filippo Orru <filippoorru@hotmail.com>
2023-12-26 13:22:06 +0100

    Highlight active section in TOC

--- themes/hugo-coder/assets/js/coder.js
+++ themes/hugo-coder/assets/js/coder.js
@@ -59,6 +59,10 @@ let toc = document.getElementById("TableOfContents");
 let content = document.getElementsByClassName("content")[0];
 let initial = getOffset(toc).top;
 
+let headers = Array.from(document.querySelectorAll("h1, h2, h3"))
+    .filter((header) => header.classList.length == 0);
+let tocElements = Array.from(toc.querySelectorAll("a"));
+
 function recalculateTocPosition() {
     if (content.classList.contains("floating")) {
         if (initial > window.scrollY + 64) {
@@ -69,6 +73,26 @@ function recalculateTocPosition() {
             content.classList.add("floating");
         }
     }
+
+    // Handle setting currently active TOC element
+    tocElements.forEach((a) => a.parentElement.classList.remove("active")); // Reset class
+
+    for (var i in headers) {
+        let header = headers[i];
+        let bottom = header.getBoundingClientRect().bottom - 300;
+
+        if (bottom > 0) {
+            let previous = i > 0 ? headers[i - 1] : header;
+            let text = previous.innerText;
+
+            let tocElement = tocElements.find((a) => a.innerText == text);
+            tocElement && tocElement.parentElement.classList.add("active");
+
+            // console.log(tocElement);
+            break;
+        }
+    }
 }
 
+recalculateTocPosition();
 window.addEventListener('scroll', recalculateTocPosition);
