4f8ab6aba5c3b001b20bae34f94698b44d8f868f
Filippo Orru <filippoorru@hotmail.com>
2024-01-24 10:30:27 +0100

    Fix toc.js

--- themes/hugo-coder/assets/js/toc.js
+++ themes/hugo-coder/assets/js/toc.js
@@ -1,59 +1,63 @@
 // This file handles the floating table of contents
 
-let toc = document.querySelector(".table-of-contents.floating #TableOfContents");
-if (!toc) {
-    // The floating TOC is not inserted if an inline TOC is present. Do nothing.
-    return;
-}
-
-function getOffset(el) {
-    var _x = 0;
-    var _y = 0;
-    while (el && !isNaN(el.offsetLeft) && !isNaN(el.offsetTop)) {
-        _x += el.offsetLeft - el.scrollLeft;
-        _y += el.offsetTop - el.scrollTop;
-        el = el.offsetParent;
-    }
-    return { top: _y, left: _x };
-}
-
-let content = document.getElementsByClassName("content")[0];
-let initial = getOffset(toc).top;
-
-let headers = Array.from(document.querySelectorAll("h1, h2, h3"))
-    .filter((header) => header.classList.length == 0);
-let tocElements = Array.from(toc.querySelectorAll("a"));
-
-function recalculateTocPosition() {
-    if (content.classList.contains("toc-floating")) {
-        if (initial > window.scrollY + 64) {
-            content.classList.remove("toc-floating");
-        }
-    } else {
-        if (toc.getBoundingClientRect().y < 64) {
-            content.classList.add("toc-floating");
-        }
+function handleFloatingToc() {
+    let toc = document.querySelector(".table-of-contents.floating #TableOfContents");
+    if (!toc) {
+        // The floating TOC is not inserted if an inline TOC is present. Do nothing.
+        return;
     }
 
-    // Handle setting currently active TOC element
-    tocElements.forEach((a) => a.parentElement.classList.remove("active")); // Reset class
+    function getOffset(el) {
+        var _x = 0;
+        var _y = 0;
+        while (el && !isNaN(el.offsetLeft) && !isNaN(el.offsetTop)) {
+            _x += el.offsetLeft - el.scrollLeft;
+            _y += el.offsetTop - el.scrollTop;
+            el = el.offsetParent;
+        }
+        return { top: _y, left: _x };
+    }
 
-    for (var i in headers) {
-        let header = headers[i];
-        let bottom = header.getBoundingClientRect().bottom - 300;
+    let content = document.getElementsByClassName("content")[0];
+    let initial = getOffset(toc).top;
 
-        if (bottom > 0) {
-            let previous = i > 0 ? headers[i - 1] : header;
-            let text = previous.innerText;
+    let headers = Array.from(document.querySelectorAll("h1, h2, h3"))
+        .filter((header) => header.classList.length == 0);
+    let tocElements = Array.from(toc.querySelectorAll("a"));
 
-            let tocElement = tocElements.find((a) => a.innerText == text);
-            tocElement && tocElement.parentElement.classList.add("active");
+    function recalculateTocPosition() {
+        if (content.classList.contains("toc-floating")) {
+            if (initial > window.scrollY + 64) {
+                content.classList.remove("toc-floating");
+            }
+        } else {
+            if (toc.getBoundingClientRect().y < 64) {
+                content.classList.add("toc-floating");
+            }
+        }
 
-            // console.log(tocElement);
-            break;
+        // Handle setting currently active TOC element
+        tocElements.forEach((a) => a.parentElement.classList.remove("active")); // Reset class
+
+        for (var i in headers) {
+            let header = headers[i];
+            let bottom = header.getBoundingClientRect().bottom - 300;
+
+            if (bottom > 0) {
+                let previous = i > 0 ? headers[i - 1] : header;
+                let text = previous.innerText;
+
+                let tocElement = tocElements.find((a) => a.innerText == text);
+                tocElement && tocElement.parentElement.classList.add("active");
+
+                // console.log(tocElement);
+                break;
+            }
         }
     }
+
+    recalculateTocPosition();
+    window.addEventListener('scroll', recalculateTocPosition);
 }
 
-recalculateTocPosition();
-window.addEventListener('scroll', recalculateTocPosition);
+handleFloatingToc();
